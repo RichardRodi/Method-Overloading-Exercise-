5834756c407718efc97d6c6118eacf04a867e176
RichardRodi <RichardmRodi@gmail.com>
2024-02-23 10:13:52 -0500

    Added Methods that repeat/overload eachother plus a bool that represents either dollar or dollars.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace MethodOverloading
 {
     public class Program
-    {
+    
+        {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+
+        }
+        public static decimal Add(decimal x, decimal y)
+
+        {
+            return x + y;
+
+        }
+        public static string Add(int x, int y, bool withDollars)
+        {
+            if (withDollars && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (withDollars && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Add(2, 2);
+            Console.WriteLine(answer);
+
+            var answer2 = Add(2.77, 2.88);
+            Console.WriteLine(answer2);
+
+            var answer3 = Add(0, 1, true);
+            Console.WriteLine(answer3);
+
+            var answer4 = Add(3, 4, true);
+            Console.WriteLine(answer4);
         }
     }
 }
